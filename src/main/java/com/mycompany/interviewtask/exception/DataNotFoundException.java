9150c8807ad27a1594bd179cd4bb1ca64f7ea764
Educational projects repository <37912789+alexivannikov@users.noreply.github.com>
2024-12-17 01:20:24 +0300

    Delete src/main directory

--- src/main/java/com/mycompany/interviewtask/exception/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.interviewtask.exception;
-
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
